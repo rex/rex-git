6b44e7b2c4eec8ad52971f31ab8478446c38ca98
Pierce Moore <me@prex.io>
2013-06-10 04:59:07 -0500

    Tests passing, ready for production

--- test/rex-git.test.js
+++ test/rex-git.test.js
@@ -1,31 +1,30 @@
-#!/usr/bin/env node
 /*###############################################################################
-#           
-#             _ __ _____  __   Welcome to the      _             
-#            | '__/ _ \ \/ / ___ __ ___ ____  _ __| |_ ___ _ __  
-#            | | |  __/>  < / -_) _/ _ (_-< || (_-<  _/ -_) '  \ 
+#
+#             _ __ _____  __   Welcome to the      _
+#            | '__/ _ \ \/ / ___ __ ___ ____  _ __| |_ ___ _ __
+#            | | |  __/>  < / -_) _/ _ (_-< || (_-<  _/ -_) '  \
 #            |_|  \___/_/\_\\___\__\___/__/\_, /__/\__\___|_|_|_|
-#                                          |__/                  
+}                                         |__/
 #
 # The rex-* ecosystem is a collection of like-minded modules for Node.js/NPM
-#   that allow developers to reduce their time spent developing by a wide margin. 
-#   
+#   that allow developers to reduce their time spent developing by a wide margin.
+#
 #   Header File Version: 0.0.1, 06/08/2013
-# 
+#
 # The MIT License (MIT)
-# 
+#
 # Copyright (c) 2013 Pierce Moore <me@prex.io>
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -33,8 +32,23 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
-# 
+#
 #######*/
-var name = require('../package.json').name
+var cli = require('rex-shell')
+  , git = require('../lib/rex-git.js')
 
-require('../lib/'+ name).init()
+cli("listing all repos in a directory")
+git.list(function(err, result) {
+  if(err) throw err
+  cli("Result from rex-git list: ", result)
+})
+
+git.status(function(err, result) {
+  if(err) throw err
+  cli("Result from rex-git status: ", result)
+})
+
+git.pull(function(err, result) {
+  if(err) throw err
+  cli("Result from rex-git pull: ", result)
+})
